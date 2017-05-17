06678ebdc5f2e89eca079884b338e44303e0df29
RenatoLobo91 <darkwolf996@gmail.com>
2017-05-17 10:55:54 +0100

    x

---
+++ php01.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>PHP01</title>
+</head>
+<body>
+<?php
+		$ola = "Hello World";
+		$num = 12;
+		$flo = 5.8;
+		$bol = TRUE;
+			echo "String : $ola </br>";
+			echo "Integer : $num </br>";
+			echo "Float : $flo </br>";
+			echo "Boolean : $bol (true = 1 and false = 0)  </br>";
+		?>
+
+</body>
+</html>
